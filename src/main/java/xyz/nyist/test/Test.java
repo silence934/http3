12523471ba704740cb3e5208ddd6916ee9926d35
silence <silence@.nyist.deu.cn>
2022-07-24 14:38:07 +0800

    init

---
+++ src/main/java/xyz/nyist/test/Test.java
@@ -0,0 +1,17 @@
+package xyz.nyist.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: fucong
+ * @Date: 2022/7/24 14:27
+ * @Description:
+ */
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        log.info("start");
+    }
+
+}
